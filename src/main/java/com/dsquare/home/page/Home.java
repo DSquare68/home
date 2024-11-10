2c0a9c9169406c53e6d6ae8c9353e1f9682bc448
Daniel Dobrzyński <35843305+DanielDob@users.noreply.github.com>
2024-11-10 19:51:32 +0100

    setting applayout

--- src/main/java/com/dsquare/home/page/Home.java
+++ src/main/java/com/dsquare/home/page/Home.java
@@ -2,14 +2,18 @@ package com.dsquare.home.page;
 
 import com.dsquare.view.CircleProgress;
 import com.dsquare.view.HomePage;
+import com.vaadin.flow.component.Unit;
 import com.vaadin.flow.component.applayout.AppLayout;
 import com.vaadin.flow.component.applayout.DrawerToggle;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.Scroller;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.component.page.Viewport;
 import com.vaadin.flow.component.sidenav.SideNav;
 import com.vaadin.flow.component.sidenav.SideNavItem;
 import com.vaadin.flow.router.PageTitle;
@@ -18,12 +22,12 @@ import com.vaadin.flow.server.StreamResource;
 
 @Route("home")
 @PageTitle("Home")
-public class Home extends AppLayout {
+//@Viewport("width=device-width, minimum-scale=1.0, initial-scale=1.0, user-scalable=yes")
+public class Home extends AppLayout  {
 	
 	private static final long serialVersionUID = 2216332923141238067L;
 
 	Home() {
-		this.setId("home");
 		DrawerToggle toggle = new DrawerToggle();
 		SideNav nav = getSideNav();
 		Scroller scroller = new Scroller(nav);
@@ -33,6 +37,8 @@ public class Home extends AppLayout {
 		addToNavbar(toggle, title);
 		addToDrawer(scroller);
 		setContent(new HomePage());
+		//this.setContent(new Span("MEOW"));
+		//this.showRouterLayoutContent(new HomePage());
 	}
 
 	private SideNav getSideNav() {
@@ -42,6 +48,7 @@ public class Home extends AppLayout {
 		Image gym = new Image(imageResource, "Gym");
         sideNav.addItem(
                 new SideNavItem(null,"/gym", gym));
+        sideNav.setWidth(8, Unit.PERCENTAGE);
         return sideNav;
 	}
 }
