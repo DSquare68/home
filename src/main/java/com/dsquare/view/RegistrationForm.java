63199853dce788e2678a19d656c0c4c9415ea6c3
Daniel Dobrzyński <35843305+DanielDob@users.noreply.github.com>
2025-02-26 20:49:03 +0100

    registration form

--- src/main/java/com/dsquare/view/RegistrationForm.java
+++ src/main/java/com/dsquare/view/RegistrationForm.java
@@ -7,6 +7,8 @@ import com.vaadin.flow.component.checkbox.Checkbox;
 import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.html.H3;
 import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.component.orderedlayout.FlexComponent.Alignment;
+import com.vaadin.flow.component.orderedlayout.FlexComponent.JustifyContentMode;
 import com.vaadin.flow.component.textfield.EmailField;
 import com.vaadin.flow.component.textfield.PasswordField;
 import com.vaadin.flow.component.textfield.TextField;
@@ -17,6 +19,7 @@ public class RegistrationForm extends FormLayout {
 
 	private H3 title;
 
+	private TextField username;
    private TextField firstName;
    private TextField lastName;
 
@@ -29,32 +32,26 @@ public class RegistrationForm extends FormLayout {
 
 
    public RegistrationForm() {
-       title = new H3("Signup form");
+       title = new H3("Sign-up");
        firstName = new TextField("First name");
+       username = new TextField("Username");
        lastName = new TextField("Last name");
        email = new EmailField("Email");
-
-       allowMarketing = new Checkbox("Allow Marketing Emails?");
-       allowMarketing.getStyle().set("margin-top", "10px");
 
        password = new PasswordField("Password");
        passwordConfirm = new PasswordField("Confirm password");
 
-       setRequiredIndicatorVisible(firstName, lastName, email, password,
+       setRequiredIndicatorVisible(username,firstName, lastName, email, password,
                passwordConfirm);
 
-       errorMessageField = new Span();
-
-       submitButton = new Button("Join the community");
+       submitButton = new Button("Register");
        submitButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
 
-       add(title, firstName, lastName, email, password,
-               passwordConfirm, allowMarketing, errorMessageField,
-               submitButton);
+       add(title,username, firstName, lastName,  password,
+               passwordConfirm,email, submitButton);
 
        // Max width of the Form
        setMaxWidth("500px");
-
        // Allow the form layout to be responsive.
        // On device widths 0-490px we have one column.
        // Otherwise, we have two columns.
@@ -65,15 +62,18 @@ public class RegistrationForm extends FormLayout {
        // These components always take full width
        setColspan(title, 2);
        setColspan(email, 2);
-       setColspan(errorMessageField, 2);
        setColspan(submitButton, 2);
+       
+       submitButton.addClickListener(e->submit());
    }
 
-   public PasswordField getPasswordField() { return password; }
+   private void submit() {
+	   
+   }
+
+public PasswordField getPasswordField() { return password; }
 
    public PasswordField getPasswordConfirmField() { return passwordConfirm; }
-
-   public Span getErrorMessageField() { return errorMessageField; }
 
    public Button getSubmitButton() { return submitButton; }
 
