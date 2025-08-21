cbdb3c19a8da9002f6790eb00e8a9d7bd3bb536f
DSquare68 <danieldob96@pm.me>
2025-08-21 19:32:01 +0200

    Training.java added

--- src/main/java/com/dsquare/model/Training.java
+++ src/main/java/com/dsquare/model/Training.java
@@ -1,6 +1,10 @@
 package com.dsquare.model;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+
+import com.dsquare.db.TrainingRecord;
+import com.dsquare.service.TrainingServiceImpl;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -9,13 +13,59 @@ import jakarta.persistence.Id;
 import jakarta.persistence.SequenceGenerator;
 import lombok.Data;
 
-@Entity
 @Data
 public class Training {
-	@Id
-	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="SEQUENCE1")
-	@SequenceGenerator(name="SEQUENCE1", sequenceName="SEQUENCE1", allocationSize=1)
-	private int id;
-	private String name;
-	private ArrayList<ExerciseNames> exercises = new ArrayList<ExerciseNames>();
+	 private int ID;
+	    private String name;
+	    private ArrayList<String> exercises;
+	    private HashMap<String,ArrayList<Round>> rounds;
+	    private boolean templete;
+
+	    public Training(int _ID, ArrayList<String> exercises, HashMap<String, ArrayList<Round>> rounds) {
+	        this.ID = _ID;
+	        this.exercises = exercises;
+	        this.rounds = rounds;
+	    }
+	    public Training(String name){
+	        this.name = name;
+	        templete = false;
+	        exercises = new ArrayList<>();
+	        rounds = new HashMap<>();
+	    }
+
+	    
+	    public class Round {
+	        int roundNumber,reps;
+	        double weight;
+
+	        public Round(int roundNumber, int reps, double weight) {
+	            this.roundNumber = roundNumber;
+	            this.reps = reps;
+	            this.weight = weight;
+	        }
+
+	        public int getRoundNumber() {
+	            return roundNumber;
+	        }
+
+	        public void setRoundNumber(int roundNumber) {
+	            this.roundNumber = roundNumber;
+	        }
+
+	        public int getReps() {
+	            return reps;
+	        }
+
+	        public void setReps(int reps) {
+	            this.reps = reps;
+	        }
+
+	        public double getWeight() {
+	            return weight;
+	        }
+
+	        public void setWeight(double weight) {
+	            this.weight = weight;
+	        }
+	    }
 }
