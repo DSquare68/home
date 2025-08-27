5013fe721d31c3717b896c229b7bc774d587d50f
DSquare68 <danieldob96@pm.me>
2025-08-27 20:45:39 +0200

    ui applayout side routing

--- src/main/java/com/dsquare/page/Gym.java
+++ src/main/java/com/dsquare/page/Gym.java
@@ -1,20 +1,31 @@
 package com.dsquare.page;
 
+import java.util.ArrayList;
+
 import com.dsquare.model.Training;
 import com.dsquare.service.ExerciseNamesServiceImpl;
 import com.dsquare.service.TrainingServiceImpl;
+import com.dsquare.view.GymTitle;
 import com.dsquare.view.TrainingOverview;
 import com.dsquare.view.TrainingsPast;
+import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.theme.Theme;
 
 @Route(layout = Home.class, value = "gym")
-public class Gym extends Home{
+public class Gym extends Div{
 	
 	private static final long serialVersionUID = 3275850456945504655L;
 	private Training schema;
+	private GymTitle title;
+	private ArrayList<Training> schemas;
 	public Gym(ExerciseNamesServiceImpl namesService, TrainingServiceImpl trainingService){
-		super(namesService,trainingService);
+		//super(namesService,trainingService);
+		schemas = trainingService.getSchemasDataTraining();
+		title = new GymTitle(schemas);
+		title.setId("gym-title-vl");
+		//setContent(title);
+		add(title);
 	}	
 }
--- src/main/java/com/dsquare/service/TrainingServiceImpl.java
+++ src/main/java/com/dsquare/service/TrainingServiceImpl.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.dsquare.db.TrainingRecord;
+import com.dsquare.model.Training;
+import com.dsquare.repository.ExerciseNamesRepository;
 import com.dsquare.repository.TrainingRepository;
 
 @Service
@@ -13,6 +15,8 @@ public class TrainingServiceImpl {
 
 	@Autowired
 	private TrainingRepository trainingRepository;
+	@Autowired
+	private ExerciseNamesServiceImpl exerciseService;
 	
 	public void addTrainingRecord(TrainingRecord record) {
 		trainingRepository.save(record);
@@ -20,5 +24,9 @@ public class TrainingServiceImpl {
 	
 	public void addTraining(ArrayList<TrainingRecord> record) {
 		trainingRepository.saveAll(record);
+	}
+
+	public ArrayList<Training> getSchemasDataTraining() {
+		return TrainingRecord.toTraininings(trainingRepository.getAllSchemas(), exerciseService);
 	}
 }
