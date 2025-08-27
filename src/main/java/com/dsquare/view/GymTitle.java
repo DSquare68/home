8f744110a88231e1c9c55faf5d122f66c3207cbb
DSquare68 <danieldob96@pm.me>
2025-08-27 20:45:39 +0200

    ui applayout side routing

--- src/main/java/com/dsquare/view/GymTitle.java
+++ src/main/java/com/dsquare/view/GymTitle.java
@@ -6,22 +6,32 @@ import java.util.List;
 import com.dsquare.model.Training;
 import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.combobox.ComboBox;
+import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 
 
 public class GymTitle extends HorizontalLayout{
 
-	private Text titlePage;
+	private Div titlePage;
 	private ComboBox schemas;
 	private ComboBox trainingsPerSchema;
-	private Label trainingName;
+	private Div trainingName;
 	public GymTitle(ArrayList<Training> trainingSchemas) {
-		titlePage = new Text("Trening's Owerview"); 
+		titlePage = new Div("Trening's Overview");
+		titlePage.setClassName("title-gym-div");
+		trainingName = new Div("Choose training");
+		trainingName.setClassName("title-gym-div");
 		schemas = new ComboBox();
+		schemas.setClassName("title-gym-combobox");
 		trainingsPerSchema = new ComboBox();
+		trainingsPerSchema.setClassName("title-gym-combobox");
 		ArrayList<String> schemasData = new ArrayList<>();
 		trainingSchemas.forEach(e->schemasData.add(e.getDate()+" "+e.getName()));
 		schemas.setItems(schemasData);
+		VerticalLayout vl = new VerticalLayout(schemas,trainingsPerSchema);
+		vl.setId("title-gym-combobox-vl");
+		add(titlePage,vl,trainingName);
 	}
 }
