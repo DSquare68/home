60c134714ab97e11abbfdff6d097872db011637b
DSquare68 <danieldob96@pm.me>
2025-09-27 19:13:20 +0200

    football date scrap and put to db

--- src/main/java/com/dsquare/repository/MatchRespository.java
+++ src/main/java/com/dsquare/repository/MatchRespository.java
@@ -1,8 +1,11 @@
 package com.dsquare.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import com.dsquare.db.MatchRecord;
 
+@Repository
 public interface MatchRespository extends JpaRepository<MatchRecord, Integer> {
 
 }
